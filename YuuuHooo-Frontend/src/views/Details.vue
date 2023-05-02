d30301266f3d442e22970777113e04ee4a923bc4
Surachet Pichaiwattanaporn <89179955+dodoza1415@users.noreply.github.com>
2023-05-02 16:04:11 +0700

    Update Details.vue

--- YuuuHooo-Frontend/src/views/Details.vue
+++ YuuuHooo-Frontend/src/views/Details.vue
@@ -16,7 +16,10 @@ onMounted(async () => {
     if (res.status === 200) {
       announcementsDetails.value = await res.json();
       console.log(announcementsDetails.value)
-    } else throw new Error("No Announcement");
+    }else if(res.status !== 200){
+      alert("The requested page is not available!") 
+      router.push('/');
+    }else throw new Error("No Announcement");
   } catch (error) {
     console.log(error);
   }
