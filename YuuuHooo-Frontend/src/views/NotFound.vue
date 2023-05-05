b9a9d0b629081a6ba900ccfb0b0966cc9a42aec9
Surachet Pichaiwattanaporn <89179955+dodoza1415@users.noreply.github.com>
2023-05-05 21:07:15 +0700

    5/5/2023

--- YuuuHooo-Frontend/src/views/NotFound.vue
+++ YuuuHooo-Frontend/src/views/NotFound.vue
@@ -1,11 +1,20 @@
-<script setup></script>
+<script setup>
+import { useRouter , RouterLink } from 'vue-router';
+const router = useRouter();
+</script>
 <template>
+  <div>
   <div class="font-['Acme']  text-[45px] text-center mt-[300px]">
   <h2>Page Not Found! </h2>
 </div>
-<div class="font-['Acme']  text-[20px] text-center">
+<div class="font-['Acme']  text-[20px] text-center mb-[50px]">
   <h5>Please check your path ( URL ).</h5>
 </div>
+<div class="btn btn-info bg-gray-200 border-transparent hover:bg-green-300 hover:border-transparent flex justify-center">
+  <RouterLink :to="{name : 'Home'}">Home</RouterLink>
+</div>
+</div>
+
 </template>
 
 <style></style>
