bd69c86627bfca839fc495b04957178a6c7c6aa8
Jemz <102203151+64130500111@users.noreply.github.com>
2023-05-15 15:49:54 +0700

    15/05/2023

--- YuuuHooo-Frontend/src/views/UserViewAnnouncement.vue
+++ YuuuHooo-Frontend/src/views/UserViewAnnouncement.vue
@@ -7,25 +7,28 @@ import { useAnnouncerStore } from "../stores/userview";
 
 const API_ROOT = import.meta.env.VITE_ROOT_API;
 
-const { params } = useRoute()
+const { params } = useRoute();
 const router = useRouter();
 const announcer = useAnnouncerStore();
 const categories = ref([]);
 const announcements = ref([]);
 
-
-const closedAnnouncements = ref('Closed Announcements');
+const closedAnnouncements = ref("Closed Announcements");
 const announcementModeTitle = () => {
   if (announcer.mode === "active") {
     closedAnnouncements.value = "Closed Announcements";
-    changeModeButton.value = "ann-button btn btn-info bg-gray-200 border-transparent hover:bg-red-300 hover:border-transparent"
+    changeModeButton.value =
+      "ann-button btn btn-info bg-gray-200 border-transparent hover:bg-red-300 hover:border-transparent";
   } else if (announcer.mode === "close") {
     closedAnnouncements.value = "Active Announcements";
-    changeModeButton.value = "ann-button btn btn-info bg-gray-200 border-transparent hover:bg-green-300 hover:border-transparent"
+    changeModeButton.value =
+      "ann-button btn btn-info bg-gray-200 border-transparent hover:bg-green-300 hover:border-transparent";
   }
-}
+};
 
-const changeModeButton = ref("ann-button btn btn-info bg-gray-200 border-transparent hover:bg-red-300 hover:border-transparent")
+const changeModeButton = ref(
+  "ann-button btn btn-info bg-gray-200 border-transparent hover:bg-red-300 hover:border-transparent"
+);
 const announcementMode = () => {
   if (announcer.mode === "active") {
     announcer.setMode("close");
@@ -50,10 +53,10 @@ const fetchAnnouncement = async () => {
     const announcer = useAnnouncerStore();
     const res = await fetch(
       `${API_ROOT}/api/announcements` +
-      "?" +
-      new URLSearchParams({
-        mode: announcer.mode,
-      })
+        "?" +
+        new URLSearchParams({
+          mode: announcer.mode,
+        })
     );
     if (res.status === 200) {
       announcements.value = await res.json();
@@ -88,14 +91,14 @@ const userDetailPage = (announcementId) => {
   router.push({ name: "UserDetails", params: { id: announcementId } });
 };
 
-const prevStatus = ref(false)
-const nextStatus = ref(false)
+const prevStatus = ref(false);
+const nextStatus = ref(false);
 // const ButtonDisabled = () => {
-  
+
 // }
 const show = () => {
-  console.log(announcements.value)
-}
+  console.log(announcements.value);
+};
 </script>
 
 <template>
@@ -105,40 +108,62 @@ const show = () => {
     </div>
     <div class="overflow-auto m-10">
       <div class="text-lg font-['Acme'] flex justify-between">
-        <span class="font-bold">Date/Time shown in Timezone &nbsp: <span class="font-thin">&nbsp{{ timezone }}</span> </span>
-      </div>
-      <div class="mb-[-10px] flex justify-between">
-      <span class="text-lg font-['Acme'] font-bold">Choose Category &nbsp:
-      <select class="ann-category-filter ml-3 mt-3 border border-gray rounded-md h-[2.0em] w-[7.5em] font-thin text-sm"
-     v-model="announcements.categoryId">
-        <option value="0">ทั้งหมด</option>
-          <option
-            :value="category.categoryId"
-            v-for="(category, index) in categories" :key="category.categoryId"
-          >
-            {{ category.categoryName }}
-          </option>
-      </select>
-    </span>
-    <span class="mb-[-20px] text-lg font-['Acme']">
-          <button @click="announcementMode(), announcementModeTitle()" :class="changeModeButton">{{ closedAnnouncements }}
-          </button>
+        <span class="font-bold"
+          >Date/Time shown in Timezone &nbsp:
+          <span class="font-thin">&nbsp{{ timezone }}</span>
         </span>
-    </div>
+      </div>
+
+      <!-- <div class="mb-[-10px] flex justify-between">
+        <span class="text-lg font-['Acme'] font-bold"
+          >Choose Category &nbsp:
+          <select
+            class="ann-category-filter ml-3 mt-3 border border-gray rounded-md h-[2.0em] w-[7.5em] font-thin text-sm"
+            v-model="announcements.categoryId"
+          >
+            <option value="0">ทั้งหมด</option>
+            <option
+              :value="category.categoryId"
+              v-for="(category, index) in categories"
+              :key="category.categoryId"
+            >
+              {{ category.categoryName }}
+            </option>
+          </select>
+        </span>
+      </div> -->
+
+      <span class="mb-[-20px] text-lg font-['Acme'] ml-[70em]">
+        <button
+          @click="announcementMode(), announcementModeTitle()"
+          :class="changeModeButton"
+        >
+          {{ closedAnnouncements }}
+        </button>
+      </span>
       <div v-if="announcements.length === 0">
         <h3 class="text-2xl font-['Acme']">No Announcement</h3>
       </div>
-      <table v-else class="table table-compact auto w-full mt-7 border shadow-lg border-gray-300  rounded-md">
+      <table
+        v-else
+        class="table table-compact auto w-full mt-7 border shadow-lg border-gray-300 rounded-md"
+      >
         <tr class="font-['Acme'] bg-red-100">
           <th class="text-lg text-center">No.</th>
           <th class="text-lg">Title</th>
           <th v-if="announcer.mode === 'close'">Close date</th>
           <th class="text-lg">Category</th>
         </tr>
-        <tr v-for="(announcement, index) in announcements" :key="announcement.id"
-          class="ann-item font-semibold border border-gray-300">
-          <td class=" text-center">{{ ++index }}</td>
-          <td class="ann-title cursor-pointer hover:text-gray-500 " @click="userDetailPage(announcement.id)">
+        <tr
+          v-for="(announcement, index) in announcements"
+          :key="announcement.id"
+          class="ann-item font-semibold border border-gray-300"
+        >
+          <td class="text-center">{{ ++index }}</td>
+          <td
+            class="ann-title cursor-pointer hover:text-gray-500"
+            @click="userDetailPage(announcement.id)"
+          >
             {{ announcement.announcementTitle }}
           </td>
           <td v-if="announcer.mode === 'close'" class="ann-close-date">
@@ -149,15 +174,13 @@ const show = () => {
           </td>
         </tr>
       </table>
-      <div class="text-2xl font-['Acme'] mt-5">
-        <!-- v-if="announcements.length >= 5" -->
+      <!-- <div class="text-2xl font-['Acme'] mt-5">
         <button class="ann-page-prev btn btn-info bg-gray-200 border-transparent hover:bg-gray-300 hover:text-slate-400 hover:border-transparent w-[80px]" :disabled="prevStatus">Prev</button>
         <button class="ann-page-0 btn btn-info bg-gray-200 border-transparent hover:bg-gray-300 hover:text-slate-400 hover:border-transparent active:bg-green-500 active:text-white w-[50px]">1</button>
         <button class="ann-page-next btn btn-info bg-gray-200 border-transparent hover:bg-green-500 hover:border-transparent hover:text-white w-[80px]" :disabled="nextStatus">Next</button>
-      </div>
-    
+      </div> -->
+    </div>
   </div>
-</div>
 </template>
 
 <style scoped></style>
