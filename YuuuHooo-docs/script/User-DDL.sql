db138db4f3a36faeb7a707aecfa1d934b6fd673b
Jemz <102203151+64130500111@users.noreply.github.com>
2023-08-20 22:09:55 +0700

    20/09/2023

---
+++ YuuuHooo-docs/script/User-DDL.sql
@@ -0,0 +1,38 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema User
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema User
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `User` DEFAULT CHARACTER SET utf8 ;
+USE `User` ;
+
+-- -----------------------------------------------------
+-- Table `User`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `User`.`user` (
+  `id` INT NOT NULL auto_increment,
+  `username` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(150) NOT NULL,
+  `role` ENUM('admin', 'announcer') NOT NULL DEFAULT 'announcer',
+  `createOn` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `updateOn` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE,
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
