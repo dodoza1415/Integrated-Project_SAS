437e3780807349f4d64ae9e34ad8ff11cff1a726
Jemz <102203151+64130500111@users.noreply.github.com>
2023-08-26 14:50:55 +0700

    26/08/2023

--- YuuuHooo-docs/script/Announcement-DDL.sql
+++ YuuuHooo-docs/script/Announcement-DDL.sql
@@ -45,6 +45,27 @@ CREATE TABLE IF NOT EXISTS `announcement`.`announcements` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+CREATE SCHEMA IF NOT EXISTS `announcement` DEFAULT CHARACTER SET utf8 ;
+USE `announcement` ;
+
+-- -----------------------------------------------------
+-- Table `User`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `announcement`.`users` (
+  `id` INT NOT NULL auto_increment,
+  `username` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(150) NOT NULL,
+  `role` ENUM('admin', 'announcer') NOT NULL DEFAULT 'announcer',
+  `createdOn` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedOn` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE,
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+ENGINE = InnoDB;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
