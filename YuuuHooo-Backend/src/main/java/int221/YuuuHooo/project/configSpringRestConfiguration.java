c90aefbb24829d1a5f202735b7551d8a7b6434dd
Jemz <102203151+64130500111@users.noreply.github.com>
2023-09-25 16:52:15 +0700

    25/09/2023

--- YuuuHooo-Backend/src/main/java/int221/YuuuHooo/project/configSpringRestConfiguration.java
+++
@@ -1,7 +0,0 @@
-package int221.YuuuHooo.project;
-
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-
-public interface configSpringRestConfiguration {
-    void configureRepositoryRestConfiguration(RepositoryRestConfiguration config);
-}
--- YuuuHooo-Backend/src/main/java/int221/YuuuHooo/project/repositories/CustomUserRepository.java
+++
@@ -1,9 +0,0 @@
-package int221.YuuuHooo.project.repositories;
-
-import int221.YuuuHooo.project.entities.User;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(exported = false)
-public interface CustomUserRepository {
-    void refresh(User user);
-}
